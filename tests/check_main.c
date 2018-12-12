bcabd20fdb6b939e08a6610e8ad5c29c7c0d4909
thomas.campistron.etu <thomas.campistron.etu@univ-lille.fr>
2018-12-12 18:36:26 +0100

    add more tests

--- tests/check_main.c
+++ tests/check_main.c
@@ -8,38 +8,116 @@ START_TEST(parse_stif_small_buffer){
 }
 END_TEST
 
+START_TEST(parse_stif_no_magic){
+	ck_assert(parse_stif((const unsigned char *) "hello", 6)==NULL);
+}
+END_TEST
+
 START_TEST(parse_stif_null_buffer){
 	ck_assert(parse_stif(NULL, 10)==NULL);
 }
 END_TEST
 
+START_TEST(parse_stif_first_block_is_data){
+	const unsigned char picture[] = {
+		0xFE, 0xCA, // MAGIC
+		// HEADER
+		0x01, // BLOCK TYPE HEADER
+		0x09, 0x00, 0x00, 0x00, // BLOCK SIZE
+		// HEADER BLOCK DATA
+		0x02, 0x00, 0x00, 0x00, // WIDTH
+		0x02, 0x00, 0x00, 0x00, // HEIGHT
+		0x00, // COLOR TYPE
+		// BLOCK DATA
+		0x01, // BLOCK TYPE DATA
+		0x01, 0x00, 0x00, 0x00, // BLOCK SIZE
+		0xFF, // PIXEL
+		// BLOCK DATA
+		0x01, // BLOCK TYPE DATA
+		0x03, 0x00, 0x00, 0x00, // BLOCK SIZE
+		0xCA, 0xCA, 0xCA // PIXEL
+	};
+	ck_assert(parse_stif(picture, sizeof(picture))==NULL);
+}
+END_TEST
+
+START_TEST(parse_stif_inconsistent_pixel_size){
+	const unsigned char picture[] = {
+		0xFE, 0xCA, // MAGIC
+		// HEADER
+		0x01, // BLOCK TYPE HEADER
+		0x09, 0x00, 0x00, 0x00, // BLOCK SIZE
+		// HEADER BLOCK DATA
+		0x02, 0x00, 0x00, 0x00, // WIDTH
+		0x02, 0x00, 0x00, 0x00, // HEIGHT
+		0x00, // COLOR TYPE
+		// BLOCK DATA
+		0x00, // BLOCK TYPE DATA
+		0x01, 0x00, 0x00, 0x00, // BLOCK SIZE
+		0xFF, // PIXEL
+		// BLOCK DATA
+		0x01, // BLOCK TYPE DATA
+		0x02, 0x00, 0x00, 0x00, // BLOCK SIZE
+		0xCA, 0xCA // PIXEL
+	};
+	ck_assert(parse_stif(picture, sizeof(picture))==NULL);
+}
+END_TEST
+
+START_TEST(parse_stif_two_headers){
+	const unsigned char picture[] = {
+		0xFE, 0xCA, // MAGIC
+		// HEADER
+		0x00, // BLOCK TYPE HEADER
+		0x09, 0x00, 0x00, 0x00, // BLOCK SIZE
+		// HEADER BLOCK DATA
+		0x02, 0x00, 0x00, 0x00, // WIDTH
+		0x02, 0x00, 0x00, 0x00, // HEIGHT
+		0x00, // COLOR TYPE
+		// BLOCK DATA
+		0x00, // BLOCK TYPE DATA
+		0x01, 0x00, 0x00, 0x00, // BLOCK SIZE
+		0xFF, // PIXEL
+		// BLOCK DATA
+		0x01, // BLOCK TYPE DATA
+		0x03, 0x00, 0x00, 0x00, // BLOCK SIZE
+		0xCA, 0xCA, 0xCA // PIXEL
+	};
+	ck_assert(parse_stif(picture, sizeof(picture))==NULL);
+}
+END_TEST
+
 Suite * parse_suite(void)
 {
-    Suite *s;
-    TCase *tc_core;
+	Suite *s;
+	TCase *tc_core;
 
-    s = suite_create("Parse");
+	s = suite_create("Parse");
 
-    /* Core test case */
-    tc_core = tcase_create("Core");
+	/* Core test case */
+	tc_core = tcase_create("Core");
 
-    tcase_add_test(tc_core, parse_stif_null_buffer);
-    tcase_add_test(tc_core, parse_stif_small_buffer);
-    suite_add_tcase(s, tc_core);
+	tcase_add_test(tc_core, parse_stif_null_buffer);
+	tcase_add_test(tc_core, parse_stif_small_buffer);
+	tcase_add_test(tc_core, parse_stif_no_magic);
+	tcase_add_test(tc_core, parse_stif_first_block_is_data);
+	tcase_add_test(tc_core, parse_stif_two_headers);
+	tcase_add_test(tc_core, parse_stif_inconsistent_pixel_size);
+	suite_add_tcase(s, tc_core);
 
-    return s;
+	return s;
 }
 
 int main(void) {
-  int no_failed = 0;                   
-  Suite *s;                            
-  SRunner *runner;                     
+	int no_failed = 0;                   
+	Suite *s;                            
+	SRunner *runner;                     
 
-  s = parse_suite();                   
-  runner = srunner_create(s);          
+	s = parse_suite();                   
+	runner = srunner_create(s);          
 
-  srunner_run_all(runner, CK_NORMAL);  
-  no_failed = srunner_ntests_failed(runner); 
-  srunner_free(runner);                      
-  return (no_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;  
+	srunner_run_all(runner, CK_NORMAL);  
+	no_failed = srunner_ntests_failed(runner); 
+	srunner_free(runner);                      
+	return (no_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;  
 }
